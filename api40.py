cad3bb633b5c6e89ebc96398e4101307e417620c
nuraydervisova <nuraydervisova@gmail.com>
2025-08-29 16:16:25 +0400

    task34.py

---
+++ api40.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def hello():
+    message = ""
+    if request.method == "POST":
+        name = request.form.get("name", "").strip()
+        if name:
+            message = f"Salam, {name}!"
+        else:
+            message = "Zehmat olamsa adinizi daxil edin!"
+
+    return render_template_string("""
+        <form method="POST">
+            Adiniz: <input type="text" name="name" /><br><br>
+            <input type="submit" value="Gonder" />
+        </form>
+        <p>{{message}}</p>                                  
+    """, message=message)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
